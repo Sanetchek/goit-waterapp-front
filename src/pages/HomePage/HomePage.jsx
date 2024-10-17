396592f1caf4d837c3ace34f388ccdcfa88b2abc
Alona93Hryhorieva <151534732+Alona93Hryhorieva@users.noreply.github.com>
2024-10-17 10:48:37 +0300

    add fix

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,11 +5,34 @@ import TodayWaterList from '../../components/TodayWaterList/TodayWaterList.jsx';
 import MonthStatsTable from '../../components/MonthStatsTable/MonthStatsTable.jsx';
 import './HomePage.css';
 
-export default function HomePage(params) {
+export default function HomePage() {
   return (
-    <div className={css.homePage}>
+    <div className={css.container}>
       HomePage
       <DailyNorma />
+      <div className={css.image}>
+        <picture>
+          <source
+            srcset="./img/about-me/foto-x1.jpg 1x, ./img/about-me/foto-x2.jpg 2x"
+            media="(min-width: 1280px)"
+          />
+          <source
+            srcset="
+            ./img/about-me/foto-tablet-x1.jpg 1x,
+            ./img/about-me/foto-tablet-x2.jpg 2x
+          "
+            media="(min-width: 768px)"
+          />
+          <source
+            srcset="
+            ./img/about-me/foto-mobil-x1.jpg 1x,
+            ./img/about-me/foto-mobil-x1.jpg 2x
+          "
+            media="(max-width: 768px)"
+          />
+          <img class="foto" src="./img/about-me/foto-x1.jpg" alt="foto" />
+        </picture>
+      </div>
       <WaterRatioPanel />
       <TodayWaterList />
       <MonthStatsTable />
