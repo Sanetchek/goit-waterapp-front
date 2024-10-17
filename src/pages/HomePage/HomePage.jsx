83280b7e66b22166ed30b47c8dfe91a634ccc530
Alona93Hryhorieva <151534732+Alona93Hryhorieva@users.noreply.github.com>
2024-10-17 10:55:53 +0300

    fix css

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import DailyNorma from '../../components/DailyNorma/DailyNorma.jsx';
 import WaterRatioPanel from '../../components/WaterRatioPanel/WaterRatioPanel.jsx';
 import TodayWaterList from '../../components/TodayWaterList/TodayWaterList.jsx';
 import MonthStatsTable from '../../components/MonthStatsTable/MonthStatsTable.jsx';
-import './HomePage.css';
+import css from './HomePage.module.css';
 
 export default function HomePage() {
   return (
