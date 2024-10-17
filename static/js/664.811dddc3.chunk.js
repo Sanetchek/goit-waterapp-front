4b98a6ca87abcfdafa979aa1e35ba7f83582b27d
Sanetchek <algryshko@gmail.com>
2024-10-17 15:53:09 +0000

    Deploying to gh-pages from @ Sanetchek/goit-waterapp-front@415c4f0f9367f79b48d42215849adf13eb8ce8ad 🚀

---
+++ static/js/664.811dddc3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[664],{664:(e,t,r)=>{r.r(t),r.d(t,{default:()=>c});var a=r(184);function c(e){return(0,a.jsx)("div",{children:"LoginPage"})}}}]);
+//# sourceMappingURL=664.811dddc3.chunk.js.map
