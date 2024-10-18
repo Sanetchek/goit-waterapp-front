7624f36acacdcf7b78832dfa16b94c9d6571a814
Alona93Hryhorieva <151534732+Alona93Hryhorieva@users.noreply.github.com>
2024-10-18 09:58:47 +0300

    add cssDayliNorm

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -11,27 +11,17 @@ import mobileImage1x from '../../assets/images/Frame@1x-mobile-min.jpg';
 import mobileImage2x from '../../assets/images/Frame@2x-mobile-min.jpg';
 
 export default function HomePage() {
-  console.log('HomePage component is rendering');
   // const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
 
   // if (!isAuthenticated) {
   //   return <h2>You must be logged in to view this page.</h2>;
   // }
 
-  // Діагностика імпортованих зображень
-  console.log('Path to bottle image:', botleImage);
-  console.log('Path to tablet image 1x:', tabletImage1x);
-  console.log('Path to tablet image 2x:', tabletImage2x);
-  console.log('Path to mobile image 1x:', mobileImage1x);
-  console.log('Path to mobile image 2x:', mobileImage2x);
-
   return (
     <div className={css.container}>
       <h1 className={css.title}>HomePage</h1>
-      <DailyNorma className={css.norma} />
-      console.log('DailyNorma component rendered');
+      <DailyNorma />
       <div className={css.image}>
-        console.log('Rendering picture element with responsive images');
         <picture>
           <source
             srcSet={`${tabletImage1x} 1x, ${tabletImage2x} 2x`}
@@ -43,25 +33,6 @@ export default function HomePage() {
           />
           <img className={css.foto} src={botleImage} alt="foto" />
         </picture>
-        {/* <picture>
-          <source
-            srcSet="../../assets/images/BotleHomescreen@1x-dec-min.jpg 1x, ../../assets/images/BotleHomescreen@2x-dec-min.jpg 2x"
-            media="(min-width: 1280px)"
-          />
-          <source
-            srcSet="../../assets/images/Frame@1x-tablet-min.jpg 1x, ../../assets/images/Frame@2x-tablet-min.jpg 2x"
-            media="(min-width: 768px)"
-          />
-          <source
-            srcSet="../../assets/images/Frame@1x-mobile-min.jpg 1x, ../../assets/images/Frame@2x-mobile-min.jpg 2x"
-            media="(max-width: 768px)"
-          />
-          <img
-            className={css.foto}
-            src="../../assets/images/BotleHomescreen@1x-dec-min.jpg"
-            alt="foto"
-          />
-        </picture> */}
       </div>
       <WaterRatioPanel />
       <TodayWaterList />
