268e3150bda6875635848cca4fad72865113adbc
Aleksandr <algryshko@gmail.com>
2024-10-25 19:46:47 +0300

    signin/ signup bottle image

--- src/pages/SigninPage/SigninPage.jsx
+++ src/pages/SigninPage/SigninPage.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import AuthContainer from '../../components/AuthContainer/AuthContainer';
 import SignInForm from '../../components/AuthForm/SigninPage/SigninForm';
-import frameImage from '../../assets/images/Frame.svg';
 import css from './SigninPage.module.css';
 
 export default function SigninPage() {
   return (
-    <AuthContainer frameImage={frameImage}>
+    <AuthContainer>
       <div className={css.container}>
         <SignInForm />
       </div>
