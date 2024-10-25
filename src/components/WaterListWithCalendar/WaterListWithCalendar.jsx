fc13ba9a9e5ae9f4fd03d891e90c6392f8751609
EldarMemetov <eldarm9347@gmail.com>
2024-10-25 14:50:30 +0200

    button-create

--- src/components/WaterListWithCalendar/WaterListWithCalendar.jsx
+++ src/components/WaterListWithCalendar/WaterListWithCalendar.jsx
@@ -6,13 +6,14 @@ const WaterListWithCalendar = ({
   openModal,
   waterConsumed,
   userDailyNormWater,
+  onEdit,
 }) => {
   return (
     <div className={styles.container}>
       <h2 className={styles.todayText}>Today</h2>
       <div className={styles.waterList}>
         {/* Pass openModal down to TodayWaterList */}
-        <TodayWaterList openModal={openModal} />
+        <TodayWaterList openModal={openModal} onEdit={onEdit} />
       </div>
       <div className={styles.calendar}>
         <MonthStatsTable
