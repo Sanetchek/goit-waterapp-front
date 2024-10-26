813df7de82984ed1ddb7b8583286e5092787abc6
Volodymyr2121 <124473843+Volodymyr2121@users.noreply.github.com>
2024-10-26 16:59:41 +0200

    adding data from db

--- src/components/DailyNorma/DailyNorma.jsx
+++ src/components/DailyNorma/DailyNorma.jsx
@@ -1,14 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import DailyNormaModal from '../DailyNormaModal/DailyNormaModal.jsx';
 import css from './DailyNorma.module.css';
 import Modal from 'components/Modal/Modal.jsx';
-import { updateDailyWaterNorm } from '../../redux/water/operations.js';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUser, updateUser } from '../../redux/user/operations.js';
+import { selectUserDailyNormWater, selectUserId } from '../../redux/auth/selectors.js';
 
 export default function DailyNorma() {
-  const [dailyNorm, setDailyNorm] = useState(2.0);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [localDailyNormWater, setLocalDailyNormWater] = useState(null);
   const dispatch = useDispatch();
+  const userId = useSelector(selectUserId);
+  const dailyNormWater = useSelector(selectUserDailyNormWater);
+
+useEffect(() => { 
+    if (userId) {
+      dispatch(getUser(userId)).then((action) => {
+        if (action.payload && action.payload.dailyNormWater) {
+          setLocalDailyNormWater(action.payload.dailyNormWater);
+        }
+      });
+    }
+  }, [dispatch, userId]);
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -19,19 +32,21 @@ export default function DailyNorma() {
   };
 
   const handleSave = (newNorm) => {
-    setDailyNorm(newNorm);
-
     const ml = newNorm * 1000;
-    const result = dispatch(updateDailyWaterNorm({ dailyNorm: ml }));
-    console.log(result);
-    setIsModalOpen(false);
+    dispatch(updateUser({
+      userId,
+      userData: { dailyNormWater: ml }
+    })).then(() => {
+      setLocalDailyNormWater(ml);
+      setIsModalOpen(false);
+    });
   };
 
   return (
     <div className={css.normaContainer}>
       <h2 className={css.title}>My daily norma</h2>
       <div className={css.normWater}>
-        {dailyNorm}L
+        {(localDailyNormWater ? localDailyNormWater : dailyNormWater) / 1000 || 2.0}L
         <button className={css.editBtn} onClick={openModal}>
           Edit
         </button>
@@ -44,16 +59,4 @@ export default function DailyNorma() {
       )}
     </div>
   );
-  //   return (
-  //     <div className={css.normaContainer}>
-  //       <h2 className={css.title}>My daily norma</h2>
-  //       <div className={css.normWater}>
-  //         {dailyNorm}L <button className={css.editBtn} onClick={handleEditClick}>Edit</button>
-  //       </div>
-  //       {isModalOpen && (<Modal title="My daily norma" onClose={closeModal}>
-  //         <DailyNormaModal onClose={closeModal} onSave={handleSave} />
-  //       </Modal>)}
-  //     </div>
-  //   );
-  // }
 }
