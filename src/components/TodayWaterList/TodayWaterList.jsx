00bf561007208899a5a50adcb8f218f60d991efa
EldarMemetov <eldarm9347@gmail.com>
2024-10-27 11:09:27 +0100

    editing-Waterlist-WaterListWithCalendar

--- src/components/TodayWaterList/TodayWaterList.jsx
+++ src/components/TodayWaterList/TodayWaterList.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import * as selectors from '../../redux/water/selectors.js';
-import snippets from "../../assets/images/snippets.svg";
+import WaterListRow from './WaterListRow/WaterListRow';
 
 import styles from './TodayWaterList.module.css';
 
-import WaterListRow from './WaterListRow/WaterListRow';
-
-export default function TodayWaterList({ openModal }) {
+export default function TodayWaterList() {
   const todaysWaterList = useSelector(selectors.selectVisibleWaterNotes);
 
   return (
@@ -20,15 +18,6 @@ export default function TodayWaterList({ openModal }) {
             </li>
           ))}
         </ul>
-      </div>
-
-      <div className={styles.addButtonContainer}>
-        <button className={styles.addButton} onClick={openModal}>
-          <svg className={styles.icon} width="24" height="24">
-            <use href={`${snippets}#icon-plus`}></use>
-          </svg>
-          <span className={styles.addText}>Add water</span>
-        </button>
       </div>
     </div>
   );
--- src/components/WaterListWithCalendar/WaterListWithCalendar.jsx
+++ src/components/WaterListWithCalendar/WaterListWithCalendar.jsx
@@ -4,7 +4,7 @@ import * as waterSelectors from '../../redux/water/selectors';
 import MonthStatsTable from '../MonthStatsTable/MonthStatsTable';
 import TodayWaterList from '../TodayWaterList/TodayWaterList';
 import Loading from '../Loading/Loading';
-
+import snippets from '../../assets/images/snippets.svg';
 import styles from './WaterListWithCalendar.module.css';
 
 const WaterListWithCalendar = ({
@@ -24,8 +24,33 @@ const WaterListWithCalendar = ({
         {!waterIsLoading && waterIsError && (
           <div className={styles.error}>Error loading water data</div>
         )}
-        {!waterIsLoading && !waterIsError && waterNotes.length > 0 && (
-          <TodayWaterList openModal={openModal} />
+        {!waterIsLoading && !waterIsError && (
+          <>
+            {waterNotes.length > 0 ? (
+              <>
+                <TodayWaterList openModal={openModal} />
+                <div className={styles.addButtonContainer}>
+                  <button className={styles.addButton} onClick={openModal}>
+                    <svg className={styles.icon} width="24" height="24">
+                      <use href={`${snippets}#icon-plus`}></use>
+                    </svg>
+                    <span className={styles.addText}>Add water</span>
+                  </button>
+                </div>
+              </>
+            ) : (
+              <div className={styles.emptyContainer}>
+                <div className={styles.addButtonContainer}>
+                  <button className={styles.addButton} onClick={openModal}>
+                    <svg className={styles.icon} width="24" height="24">
+                      <use href={`${snippets}#icon-plus`}></use>
+                    </svg>
+                    <span className={styles.addText}>Add water</span>
+                  </button>
+                </div>
+              </div>
+            )}
+          </>
         )}
       </div>
       <div className={styles.calendar}>
