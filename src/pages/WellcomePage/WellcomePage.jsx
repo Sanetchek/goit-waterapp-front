9e0b1e4ea546352fa347970f448c7c2898ad1617
Leoyalta <149539157+Leoyalta@users.noreply.github.com>
2024-10-27 11:12:28 +0100

    wellcome-page-mobile-add-query

--- src/pages/WellcomePage/WellcomePage.jsx
+++ src/pages/WellcomePage/WellcomePage.jsx
@@ -1,6 +1,6 @@
 import Main from '../../components/Main/Main.jsx';
 import css from './WellcomePage.module.css';
-export default function WellcomePage(params) {
+export default function WellcomePage() {
   return (
     <section className={css.mainContainer}>
       <div className={css.mainWrapperBg}>
