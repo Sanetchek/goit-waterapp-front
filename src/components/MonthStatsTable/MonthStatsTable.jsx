d765fd39267efb27fd89013385a63fb98abe9443
EldarMemetov <eldarm9347@gmail.com>
2024-10-28 10:21:51 +0100

    calendar

--- src/components/MonthStatsTable/MonthStatsTable.jsx
+++ src/components/MonthStatsTable/MonthStatsTable.jsx
@@ -1,53 +1,44 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io';
 import css from './MonthStatsTable.module.css';
-// import { useDispatch } from 'react-redux';
-// import { fetchMonthlyWaterConsumption } from '../../redux/water/operations';
-
-// import DaysGeneralStats from '../DaysGeneralStats/DaysGeneralStats';
 import CalendarItem from '../CalendarItem/CalendarItem';
 
-const MonthStatsTable = ({ monthlyWaterlist }) => {
-  // const dispatch = useDispatch();
-  const date = new Date();
-
-  const [currentYear, setCurrentYear] = useState(date.getFullYear());
-  const [currentMonth, setCurrentMonth] = useState(date.getMonth()+1);
+const MonthStatsTable = ({
+  monthlyWaterlist,
+  currentYear,
+  currentMonth,
+  onMonthChange,
+}) => {
   const [activePopup, setActivePopup] = useState(null);
-  // const [fetched, setFetched] = useState(false);
 
   const handlePopupToggle = dayObject => {
     setActivePopup(prev => (prev === dayObject ? null : dayObject));
   };
 
   const handlePrevMonth = () => {
-    setCurrentMonth(prev => {
-      const newMonth = prev === 0 ? 11 : prev - 1;
-      if (prev === 0) setCurrentYear(year => year - 1);
-      console.log('Previous month:', newMonth, 'Year:', currentYear);
-      return newMonth;
-    });
+    const newMonth = currentMonth === 1 ? 12 : currentMonth - 1;
+    const newYear = currentMonth === 1 ? currentYear - 1 : currentYear;
+    onMonthChange(newYear, newMonth);
   };
 
   const handleNextMonth = () => {
-    setCurrentMonth(prev => {
-      const newMonth = prev === 11 ? 0 : prev + 1;
-      if (prev === 11) setCurrentYear(year => year + 1);
-      console.log('Next month:', newMonth, 'Year:', currentYear);
-      return newMonth;
-    });
+    const newMonth = currentMonth === 12 ? 1 : currentMonth + 1;
+    const newYear = currentMonth === 12 ? currentYear + 1 : currentYear;
+    onMonthChange(newYear, newMonth);
   };
 
-  // useEffect(() => {
-  //   const dataExists =
-  //     monthlyWaterlist?.year === currentYear &&
-  //     monthlyWaterlist?.month === currentMonth;
+  useEffect(() => {
+    const fetchData = () => {
+      console.log(
+        'Fetching data for year:',
+        currentYear,
+        'month:',
+        currentMonth
+      );
+    };
 
-  //   if (!dataExists) {
-  //     console.log("Fetching data for year:", currentYear, "month:", currentMonth);
-  //     dispatch(fetchMonthlyWaterConsumption({ year: currentYear, month: currentMonth }));
-  //   }
-  // }, [dispatch, currentYear, currentMonth, monthlyWaterlist]);
+    fetchData();
+  }, [currentYear, currentMonth]);
 
   return (
     <>
@@ -58,14 +49,14 @@ const MonthStatsTable = ({ monthlyWaterlist }) => {
             <IoIosArrowBack />
           </button>
           <p className={css.month}>
-            {new Date(currentYear, currentMonth).toLocaleString('en-US', {
+            {new Date(currentYear, currentMonth - 1).toLocaleString('en-US', {
               month: 'long',
             })}
           </p>
           <p className={css.year}>{currentYear}</p>
           {currentYear < new Date().getFullYear() ||
           (currentYear === new Date().getFullYear() &&
-            currentMonth < new Date().getMonth()) ? (
+            currentMonth < new Date().getMonth() + 1) ? (
             <button className={css.nextButton} onClick={handleNextMonth}>
               <IoIosArrowForward />
             </button>
@@ -80,7 +71,7 @@ const MonthStatsTable = ({ monthlyWaterlist }) => {
           <ul className={css.daysList}>
             {monthlyWaterlist.length > 0 &&
               monthlyWaterlist.map(day => (
-                <li className="itemWrapper" key={day.key}>
+                <li className={css.itemWrapper} key={day.key}>
                   <CalendarItem
                     dayObject={day}
                     onPopupToggle={handlePopupToggle}
