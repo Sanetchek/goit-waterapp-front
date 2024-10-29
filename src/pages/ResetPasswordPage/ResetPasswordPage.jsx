3a9f142c782d852d177e1dd2738f9d9c5b2f59fc
Aleksandr <algryshko@gmail.com>
2024-10-29 15:27:47 +0200

    reset pass

--- src/pages/ResetPasswordPage/ResetPasswordPage.jsx
+++ src/pages/ResetPasswordPage/ResetPasswordPage.jsx
@@ -1,21 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import AuthContainer from '../../components/AuthContainer/AuthContainer';
 import ResetPasswordForm from '../../components/AuthForm/ResetPasswordPage/ResetPasswordForm';
-import { useNavigate, useSearchParams } from 'react-router-dom';
-import { selectPasswordReset } from '../../redux/auth/selectors';
-import { useSelector } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
 
 export default function ResetPasswordPage() {
   const [searchParams] = useSearchParams();
   const token = searchParams.get('token');
-  const navigate = useNavigate();
-  const passwordReset = useSelector(selectPasswordReset);
-
-  useEffect(() => {
-    if (passwordReset === true) {
-      navigate('/');
-    }
-  }, [passwordReset, navigate]);
 
   return (
     <AuthContainer>
