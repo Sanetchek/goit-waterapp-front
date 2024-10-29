6d27c966141430701c56e47e9df448e72291adad
NastyaTron <gabylia445@gmail.com>
2024-10-29 10:40:01 +0200

    add resetPassword

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import AppBar from '../AppBar/AppBar';
-import css from './Layout.module.css';
-import { useSelector } from 'react-redux';
-import { selectUserThemeColor } from '../../redux/auth/selectors';
-import clsx from 'clsx';
+// import css from './Layout.module.css';
+// import { useSelector } from 'react-redux';
+// import { selectUserThemeColor } from '../../redux/auth/selectors';
+// import clsx from 'clsx';
 
 const Layout = ({ children, username }) => {
-  const themeColor = useSelector(selectUserThemeColor);
-  const layoutClasses = clsx(css.layout, themeColor === 'dark' ? css.dark : css.light);
+  // const themeColor = useSelector(selectUserThemeColor);
+  // const layoutClasses = clsx(
+  //   css.layout,
+  //   themeColor === 'dark' ? css.dark : css.light
+  // );
 
   return (
-    <div className={layoutClasses}>
+    <div>
       <AppBar username={username} />
       <main>{children}</main>
     </div>
